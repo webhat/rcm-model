2c2a87771663b85fd09d798b3aebc29ab7a48ee2
Daniël W. Crompton <daniel.crompton@gmail.com>
2013-02-09 22:09:55 +0100

    Add Node

--- php/RCMModel.class.php
+++ php/RCMModel.class.php
@@ -6,18 +6,36 @@ use Everyman\Neo4j\Client,
 
 class RCMModel {
 
-	private $config = null;
-	private $client = null;
+	private $config			= null;
+	private $client			= null;
+	private $nodeIndex	= null;
 
 	public function __construct() {
 		$this->setConfig( new RCMConfig());
 		$this->setClient( new Client());
+		$this->setNodeIndex( new NodeIndex($this->getClient(), 'NI'));
 	}
 
 	public function addNode( $name, $properties = array()) {
-		$ret = $this->client->makeNode()->setProperty( 'name', $name)->save();
+		$ret = $this->client->makeNode()->setProperty( 'name', $name);
 
-		if($ret->getProperty('name') == $name);
+		if(is_array($properties) && !empty($properties)) {
+			foreach( $properties as $key => $value)
+				$ret->setProperty($key, $value);
+		}
+
+		$ret = $ret->save();
+		$this->nodeIndex->add($ret, 'name', $ret->getProperty('name'));
+
+		return ($ret->getProperty('name') == $name);
+	}
+
+	public function setNodeIndex($nodeIndex) {
+		return $this->nodeIndex = $nodeIndex;
+	}
+
+	public function getNodeIndex() {
+		return $this->nodeIndex;
 	}
 
 	public function setConfig($config) {
--- php/test/RCMModelTest.class.php
+++ php/test/RCMModelTest.class.php
@@ -1,42 +1,119 @@
 <?php
 
+use	Everyman\Neo4j\Cypher;
+
 class RCMModelTest extends PHPUnit_Framework_TestCase {
 
 	private $rcmm		= null;
 
 	/* Mocked Objects */
+	private $propertyContainer	= null;
+	private $nodeIndex	= null;
 	private $client	= null;
+	private $realClient	= null;
 	private $node		= null;
 
 	public function setUp() {
+		$this->nodeIndex = $this->getMock('NodeIndex', array('add'));
 		$this->client = $this->getMock('Client', array('makeNode'));
 		$this->node = $this->getMock('Node', array('setProperty'));
-		$this->propertycontainer = $this->getMock('PropertyContainer', array('save', 'getProperty'));
+		$this->propertyContainer = $this->getMock('PropertyContainer', array('save', 'getProperty'));
 		$this->rcmm = new RCMModel();
-		$this->rcmm->setClient($this->client);
+		$this->realClient = $this->rcmm->getClient();
 	}
 
 	public function tearDown() {
+
+		// Clean Up Server
+		try {
+			$this->realClient->getServerInfo(true);
+		} catch( Exception $e ) {
+			return;
+		}
 	}
 
 	/**
 	 * @dataProvider nodeProvider
 	 */
-	public function testAddNodeNoProperties( $nodeName, $expected) {
-		$this->propertycontainer->expects($this->once())->method('getProperty');
-		$this->propertycontainer->expects($this->once())->method('save')->will($this->returnValue($this->propertycontainer));
-		$this->node->expects($this->once())->method('setProperty')->will($this->returnValue($this->propertycontainer));
+	public function testServerAddNodeWithProperties( $nodeName, $properties, $expected) {
+		$this->rcmm->setClient($this->client);
+		$this->rcmm->setNodeIndex($this->nodeIndex);
+
+		$this->propertyContainer->expects($this->atLeastOnce())->method('getProperty')->will($this->returnValue(true));
+		$this->propertyContainer->expects($this->atLeastOnce())->method('save')->will($this->returnValue($this->propertyContainer));
+		$this->node->expects($this->atLeastOnce())->method('setProperty')->will($this->returnValue($this->propertyContainer));
 		$this->client->expects($this->once())->method('makeNode')->will($this->returnValue($this->node));
+
 		$actual = $this->rcmm->addNode( $nodeName);
 
 		$this->assertEquals( $expected, $actual);
 	}
 
+
+	/**
+	 * @dataProvider nodeProvider
+	 */
+	public function testAddNodeNoProperties( $nodeName, $properties, $expected) {
+		$this->rcmm->setClient($this->client);
+		$this->rcmm->setNodeIndex($this->nodeIndex);
+
+		$this->propertyContainer->expects($this->exactly(2))->method('getProperty')->will($this->returnValue(true));
+		$this->propertyContainer->expects($this->once())->method('save')->will($this->returnValue($this->propertyContainer));
+		$this->node->expects($this->once())->method('setProperty')->will($this->returnValue($this->propertyContainer));
+		$this->client->expects($this->once())->method('makeNode')->will($this->returnValue($this->node));
+
+		$actual = $this->rcmm->addNode( $nodeName);
+
+		$this->assertEquals( $expected, $actual);
+	}
+
+	/**
+	 * @dataProvider nodeProvider
+	 */
+	public function testServerAddNodeNoProperties( $nodeName, $properties, $expected) {
+		try {
+			$this->rcmm->setClient($this->realClient);
+			$this->rcmm->getClient()->getServerInfo(true);
+		} catch( Exception $e ) {
+			$this->markTestSkipped('neo4j Server down');
+		}
+
+		$actual = $this->rcmm->addNode( $nodeName);
+		$this->assertEquals( $expected, $actual);
+
+		$queryTemplate = "START root=node:NI('name:$nodeName') ".
+			"RETURN root";
+		$query = new Cypher\Query( $this->rcmm->getClient(), $queryTemplate, array('name' => $nodeName));
+		$result = $query->getResultSet();
+
+		foreach($result as $row) {
+			$this->rcmm->getClient()->deleteNode($row['root']);
+		}
+
+	}
+
+
 	public function nodeProvider() {
 		return array( // elements
 				array( // args
 					"NodeName1", // var
-					false // expected
+					array(),
+					true // expected
+					),
+				array( // args
+					"NodeName2", // var
+					array("propkey2" => "propval2"),
+					true // expected
+					),
+				array( // args
+					"NodeName3", // var
+					array(
+						"propkey31" => "propval31",
+						"propkey32" => "propval32",
+						"propkey33" => "propval33",
+						"propkey34" => "propval34"
+						),
+					true // expected
 					)
 				);
 	}
